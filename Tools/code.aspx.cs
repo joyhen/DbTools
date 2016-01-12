bf1a01a0b2e7c2e8c0822f2ab30b739904021d90
jon <573741776@qq.com>
2016-01-12 19:10:51 +0800

    简单代码生成功能改进

--- Tools/code.aspx.cs
+++ Tools/code.aspx.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 namespace Tools
 {
     using Help;
+    using System.Data.SqlClient;
+    using System.Data;
 
     public partial class code : BasePage
     {
@@ -22,7 +24,59 @@ namespace Tools
                 var arr = field.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
                 coderesult = GenCode(table, arr);
                 coderesult += string.Format(getdatatemplate.GenCodeFormat(), table, field);
+
+                var col = string.Join(",", arr.Select(x => string.Format("[{0}]", x)).ToArray());
+                var val = string.Join(",", arr.Select(x => string.Format("@{0}", x)).ToArray());
+                coderesult += string.Format(insertsql, table, col, val, getSqlParameter(arr, table), setSqlParameter(arr));
+
             }
+        }
+
+        /// <summary>
+        /// sqlserver 数据库字段类型集合
+        /// </summary>
+        private List<keyvalue> Sqltype = Enum
+            .GetNames(typeof(SqlDbType))
+            .Select(x => new keyvalue { key = x, val = x.ToLower() }).ToList();
+
+        private string getSqlParameter(string[] arr, string table)
+        {
+            var query = arr.Select(x =>
+            {
+                var thistype = getcurrenttype(table, x);
+                var maptype = Sqltype.FirstOrDefault(t => string.Compare(t.val, thistype, ignoreCase: true) == 0); //映射到数据库
+                return string.Format("new SqlParameter(\"@{0}\", SqlDbType.{1})", x, maptype.key);
+            });
+
+            return string.Join(@",
+        ", query);
+        }
+        private string setSqlParameter(string[] arr)
+        {
+            var ps = new System.Text.StringBuilder(System.Environment.NewLine);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ps.AppendLine(string.Format("    parameters[{0}] = paramArg.{1};", i, arr[i]));
+            }
+            return ps.ToString();
+        }
+
+        /// <summary>
+        /// 获取当前列的映射类型（小写的）
+        /// </summary>
+        private string getcurrenttype(string table, string column)
+        {
+            var cachedata = GetTableDetail(table);
+            if (cachedata == null) return "";
+
+            var tbiinfo = cachedata.FirstOrDefault(x => x.name == column);
+            return tbiinfo == null ? "" : tbiinfo.type;
+        }
+
+        internal class keyvalue
+        {
+            public string key { get; set; }
+            public string val { get; set; }
         }
 
         private string GenCode(string table, string[] fields)
@@ -63,13 +117,13 @@ namespace Tools
             {
                 return @"
 
-public List<{0}> Get{0}Data()
+public List<DTO_{0}> GetDTO_{0}Data()
 {{
     var sql = ""SELECT {1} FROM {0}"";
     var ds = DBHelp.ExecuteDataSet(sql);
     if (CheckDS(ds))
     {{
-        var result = ds.Tables[0].MapTo<{0}>();
+        var result = ds.Tables[0].MapTo<DTO_{0}>();
         return result.ToList();
     }}
     return null;
@@ -121,11 +175,35 @@ public List<{0}> Get{0}Data()
                 return @"
 using System;
 
-public class {0}
-{{
-    {1}
+public class DTO_{0}
+{{  {1}
 }}";
             }
         }
+
+        private string insertsql
+        {
+            get
+            {
+                return @"
+
+private bool Insert_{0}(DTO_{0} paramArg)
+{{
+    string sql = ""INSERT INTO [{0}]({1})VALUES({2})"";
+
+    SqlParameter[] parameters = new SqlParameter[]
+    {{
+        {3}
+    }};
+    {4}
+    var result = DBHelp.ExecuteSql(sql, parameters);
+    return result > 0;
+}}";
+                //return "";
+            }
+        }
+
+        //...
+
     }
 }
